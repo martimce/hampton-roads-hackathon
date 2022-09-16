800ec6408b2ed8e5892700fdee6575ae12370e8a
McElreath Marti <mcelreathmarti@McElreaths-MBP.home>
2022-09-15 21:21:16 -0700

    Updates

---
+++ precache-manifest.a72638174e6e8cf23c638f1d80bcb5af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a32ee9755e0e2c1d368ba51c947b45",
+    "url": "/hampton-roads-hackathon/index.html"
+  },
+  {
+    "revision": "1da8a538a5975b3e66ec",
+    "url": "/hampton-roads-hackathon/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3e8267903ecb85bf5a63",
+    "url": "/hampton-roads-hackathon/static/js/2.dfc24146.chunk.js"
+  },
+  {
+    "revision": "493b9e83935122e55f356f0d3cbcfa08",
+    "url": "/hampton-roads-hackathon/static/js/2.dfc24146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da8a538a5975b3e66ec",
+    "url": "/hampton-roads-hackathon/static/js/main.eceaa621.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/hampton-roads-hackathon/static/js/main.eceaa621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7527d8194cd9bf5f8235",
+    "url": "/hampton-roads-hackathon/static/js/runtime-main.deaa2973.js"
+  },
+  {
+    "revision": "5ad40f67f6405e0ad2ee9bb871bcdc5d",
+    "url": "/hampton-roads-hackathon/static/media/bird_data.5ad40f67.csv"
+  },
+  {
+    "revision": "caca60dcedd679339934c63a9d38eca7",
+    "url": "/hampton-roads-hackathon/static/media/bird_observations.caca60dc.csv"
+  },
+  {
+    "revision": "04429955175bd398a8931215856efc60",
+    "url": "/hampton-roads-hackathon/static/media/logo.04429955.JPG"
+  }
+]);
